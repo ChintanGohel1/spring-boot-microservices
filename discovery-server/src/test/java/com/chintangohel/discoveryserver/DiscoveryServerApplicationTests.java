7d78b1b10c2f96dc78abac6ac9a92065a64e103a
ChintanGohel1 <chintan.gohel@ics-global.in>
2019-11-07 14:33:26 +0530

    initial commit

--- discovery-server/src/test/java/com/chintangohel/discoveryserver/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/chintangohel/discoveryserver/DiscoveryServerApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class DiscoveryServerApplicationTests {
+public class DiscoveryServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
