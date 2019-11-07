d1bc3a6df57d8105ca4095380578369a3fecf4cc
ChintanGohel1 <chintan.gohel@ics-global.in>
2019-11-07 14:27:23 +0530

    initial commit

--- restaurant-catalog-service/src/main/java/com/chintangohel/restaurantcatalogservice/restaurant/controller/RestaurantCatalogController.java
+++ restaurant-catalog-service/src/main/java/com/chintangohel/restaurantcatalogservice/restaurant/controller/RestaurantCatalogController.java
@@ -1,6 +1,5 @@
 package com.chintangohel.restaurantcatalogservice.restaurant.controller;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
